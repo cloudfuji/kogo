c3c2488c20d7d73cc1a68ec8c549d34472d6aba7
Akash Manohar J <akash@akash.im>
2011-12-13 03:15:25 +0530

    Reverts back cas url to noshido
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,8 +3,7 @@
 Devise.setup do |config|
 
   if Rails.env.development?
-    # config.cas_base_url = "https://noshido.com:5000/cas"
-    config.cas_base_url = "https://bushi.do/cas"
+    config.cas_base_url = "https://noshido.com:5000/cas"
   end
   
   # ==> Mailer Configuration
