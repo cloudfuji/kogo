b8d22053708e91814f3cae85cd9061ac175175b3
Sean Grove <s@bushi.do>
2011-12-31 12:59:43 -0800

    Works with new AWS IAM STS federated credential system

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -5,11 +5,18 @@ class Attachment < ActiveRecord::Base
   belongs_to :user
   belongs_to :message
 
+  ap ENV
+
   has_attached_file :file,
-                    :storage        => :s3,
-                    :s3_credentials => "config/s3.yml",
-                    :url            => "/:attachment/:id/:style/:basename.:extension",
-                    :path           => "#{ENV['S3_PREFIX']}/:attachment/:id/:style/:basename.:extension"
+                    :storage         => :s3,
+                    :s3_credentials  => {
+                      :access_key_id     => ENV['S3_ACCESS_KEY_ID'],
+                      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY'],
+                      :session_token     => ENV['STS_SESSION_TOKEN']
+                    },
+                    :bucket            => ENV['S3_BUCKET'],
+                    :url             => "/:attachment/:id/:style/:basename.:extension",
+                    :path            => "#{ENV['S3_PREFIX']}/:attachment/:id/:style/:basename.:extension"
 
   attr_accessible :file
 
