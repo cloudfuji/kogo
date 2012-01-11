2db5b05eb3f6589adac9a7de7ecd04ab7922a8c8
Akash Manohar J <akash@akash.im>
2012-01-12 04:03:29 +0530

    Adds user_updated hook
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- lib/bushido/hooks/user_hooks.rb
+++ lib/bushido/hooks/user_hooks.rb
@@ -8,4 +8,18 @@ class BushidoUserHooks < Bushido::EventObserver
   def user_removed
     User.find_by_ido_id(params['data']['ido_id']).try(:disable!)
   end
+
+  def user_updated
+    data = params['data']
+    user = User.find_by_ido_id(data['ido_id'])
+    if user
+      user.update_attributes({
+          :email      => data['email'],
+          :first_name => data['first_name'],
+          :last_name  => data['last_name'],
+          :locale     => data['locale'],
+          :timezone   => data['timezone']
+        })
+    end
+  end
 end
