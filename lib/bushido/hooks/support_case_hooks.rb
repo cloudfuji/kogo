5e742e91daaae212a83a26c5819fd9cda6b00e73
Sean Grove <s@bushi.do>
2012-02-03 11:31:20 -0800

    Adds some debug output for the support_case_opened event hook

--- lib/bushido/hooks/support_case_hooks.rb
+++ lib/bushido/hooks/support_case_hooks.rb
@@ -15,18 +15,31 @@ class BushidoSupportCaseHooks < Bushido::EventObserver
   # }
 
   def support_case_opened
+    puts params.inspect
+
     data   = params['data']
     author = data['author']
     email  = author['email']
     name   = " (#{author['first_name']} #{author['last_name']})" unless author.nil? || author['first_name'].blank?
     name ||= ""
+
+    puts data.inspect
+    puts "Author: #{author.inspect} (nil? #{author.nil?})"
+    puts "\tauthor['first_name']: #{author['first_name']} (blank? #{author['first_name'].blank?})"
+    puts "\tname: #{name}"
+
     title  = data['title']
     source = data['source']
+
+    puts "title: #{title}"
+    puts "source: #{source}"
 
     message = "New support case opened: #{email}"
     message += name
     message += " [via #{source}]"
     message += " says, '#{title}'"
+
+    puts "message: #{message}"
 
     Channel.announce(message)
   end
