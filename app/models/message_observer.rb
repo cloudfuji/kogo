3da4159958bc41fa4bb0acc0e7f04e42e03b615f
Sean Grove <s@bushi.do>
2012-03-08 16:27:41 -0800

    Adds the @all message syntax for notifications

--- app/models/message_observer.rb
+++ app/models/message_observer.rb
@@ -2,12 +2,18 @@ class MessageObserver < ActiveRecord::Observer
   observe :message
 
   def after_create(message)
-    User.all.each do |user|
-      if !user.name.blank? && message.content.downcase.include?(user.name.downcase) 
-        print "Includes user name #{user.name}"
-        if message.user_id != user.id # Don't notify the user if they typed their own name
-          puts "Sending notification"
-          user.notify(message.channel.name, "#{message.user.name}: #{message.content}", "chat")
+    if message.downcase.include?("@all")
+      message.channel.users.keys.collect do |user_id|
+        User.find(id).notify(message.channel.name, "#{message.user.name}: #{message.content}", "chat")
+      end
+    else
+      User.all.each do |user|
+        if !user.name.blank? && message.content.downcase.include?(user.name.downcase) 
+          print "Includes user name #{user.name}"
+          if message.user_id != user.id # Don't notify the user if they typed their own name
+            puts "Sending notification"
+            user.notify(message.channel.name, "#{message.user.name}: #{message.content}", "chat")
+          end
         end
       end
     end
