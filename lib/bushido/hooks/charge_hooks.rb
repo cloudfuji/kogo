484c80aee2159cbc6ccef34edc6a6d442a0a4285
Sean Grove <s@bushi.do>
2012-03-14 12:59:16 -0700

    Slight tweak to the charge hook behavior to have it also announce the charge event

--- lib/bushido/hooks/charge_hooks.rb
+++ lib/bushido/hooks/charge_hooks.rb
@@ -3,5 +3,6 @@ class BushidoChargeHooks < Bushido::EventObserver
     # The catch-all even will take care of outputting the human
     # readable form of this, we just play the gong sound here
     Channel.announce("/play gong")
+    Channel.announce(params['data']['human']) unless params['data']['human'].nil?
   end
 end
