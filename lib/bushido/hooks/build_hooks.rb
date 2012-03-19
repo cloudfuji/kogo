8c0fbe02e3eea7bbf452426189f653ab2f2c93a4
Sean Grove <s@bushi.do>
2012-03-19 15:49:34 -0700

    Announce the build success in Kogo

--- lib/bushido/hooks/build_hooks.rb
+++ lib/bushido/hooks/build_hooks.rb
@@ -1,5 +1,6 @@
 class BushidoBuildHooks < Bushido::EventObserver
   def build_success
     Channel.announce("/play perfect")
+    Channel.announce(params['data']['human']) unless params['data']['human'].nil?
   end
 end
